b91677fb8644acad6547b017f813e1293edad96a
Guillem Hernandez Sola <guillem@agile611.com>
2022-09-15 17:39:56 +0200

    Added java 8

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 apt-get update
-apt-get -y install openjdk-9-jdk openjdk-9-jre
+apt-get -y install openjdk-8-jdk openjdk-8-jre
 #wget https://downloads.sourceforge.net/project/sonar-pkg/deb/binary/sonar_6.7.4_all.deb
 wget https://sourceforge.net/projects/sonar-pkg/files/deb/binary/sonar_7.1_all.deb
 dpkg -i sonar_7.1_all.deb
