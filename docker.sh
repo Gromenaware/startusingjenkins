f0ab8d2bd8a29c0c210c920677a23fa4e2dfb86c
Guillem Hernandez Sola <guillem@agile611.com>
2022-09-15 14:39:02 +0200

    Environment finished

--- docker.sh
+++ docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -eux
-echo "deb http://http.debian.net/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
 apt-get update
 apt-get install -y --no-install-recommends \
         apt-transport-https \
