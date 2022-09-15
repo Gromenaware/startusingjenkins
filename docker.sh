3d008d6f06ea736872ce210592099e25b16a1ab8
Guillem Hernandez Sola <guillem@agile611.com>
2022-09-15 14:32:31 +0200

    Jessie Backports out

--- docker.sh
+++ docker.sh
@@ -2,7 +2,6 @@
 set -eux
 echo "deb http://http.debian.net/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
 apt-get update
-apt-get -y install -t jessie-backports 
 apt-get install -y --no-install-recommends \
         apt-transport-https \
         ca-certificates \
